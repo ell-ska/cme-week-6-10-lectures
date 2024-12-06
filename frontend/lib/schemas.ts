e58c79c2b30030e0b4f1cd98af28d1b1e75add3b
ell-ska <ella@skagerstrom.se>
2024-12-06 12:52:15 +0100

    friday week 7

--- frontend/lib/schemas.ts
+++ frontend/lib/schemas.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+// actions
+
 export const signUpSchema = z.object({
   username: z.string().min(2, 'username must be at least 2 characters'),
   password: z.string().min(6, 'password must be at least 6 characters'),
@@ -13,3 +15,12 @@ export const logInSchema = z.object({
 })
 
 export type LogInValues = z.infer<typeof logInSchema>
+
+// data
+
+export const profileSchema = z.object({
+  username: z.string(),
+  id: z.string(),
+})
+
+export type ProfileData = z.infer<typeof profileSchema>
